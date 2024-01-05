73a80a14dcbdd853a78f5ba2628e38fe953d08d1
Michel Lentzler <michel.lentzler@gmail.com>
2024-01-05 12:08:09 +0100

    feat: got rid of no Todos text

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -7,7 +7,6 @@ export function TodoList({ todos, checkItem, deleteTodo }) {
         <>
         <h1 className="header">Todo List</h1>
         <ul className="list">
-            {todos.length === 0 && "No Todos"}
             {todos.map(todo => {
             return (
                 <TodoItem

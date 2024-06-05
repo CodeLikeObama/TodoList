3372749e05f23f801da8c891282951d47f650c8d
Michel <>
2024-06-05 12:01:21 +0200

    feat: style checkbox and delete button

--- src/AddItems.jsx
+++ src/AddItems.jsx
@@ -1,32 +1,31 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export function AddItems({ onSubmit }) {
-    const [newItem, setNewItem] = useState("")
-    
-    function handleSubmit(e) {
-        e.preventDefault()
-        
-        if(newItem === "") return
+  const [newItem, setNewItem] = useState("");
 
-        onSubmit(newItem)
+  function handleSubmit(e) {
+    e.preventDefault();
 
-        setNewItem("")
-        }
-    
-    return (
-        <form onSubmit={handleSubmit} className="new-items-form">
-            <div className="row">
-                <label htmlFor="item">New Item</label>
-                    <input 
-                    value={newItem} 
-                    onChange={event => setNewItem(event.target.value)} 
-                    type="text" 
-                    className="inputField" 
-                    id="item">
-                </input>
-            </div>
-                <button className="addBtn">Add</button>
-        </form>
-    )
+    if (newItem === "") return;
+
+    onSubmit(newItem);
+
+    setNewItem("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="new-items-form">
+      <div className="row">
+        <label htmlFor="item">New Item</label>
+        <input
+          value={newItem}
+          onChange={(event) => setNewItem(event.target.value)}
+          type="text"
+          className="inputField"
+          id="item"
+        ></input>
+      </div>
+      <button className="addBtn">Add</button>
+    </form>
+  );
 }
-
